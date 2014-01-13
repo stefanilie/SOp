fd88bca1469b08a9708ddf8299605c4106840483
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-01-13 11:00:13 +0200

    moloz la tav

--- F13.c
+++ F13.c
@@ -5,37 +5,33 @@
 int main(int argc, char *argv[]){
 
 	if(argc != 3){
-		fprintf(stderr, "Utilizare: %s numele caracterului si numele fisierului \n", argv[0]);
+		fprintf(stderr, "Utilizare: %s caracter fisier \n", argv[0]);
 		return 1;
 	}
 
-	char moloz; 
-        FILE *f;
-        f = fopen(argv[2], "r");
-        if(!f){
-           perror(argv[2]);
-        }
-        
-        moloz = argv[1][0];
+  char moloz; 
+  char str;
+  FILE *f;
+  f = fopen(argv[2], "r");
+  if(!f)
+  {
+     perror(argv[2]);
+     return -1;
+  }
+  
+  moloz = argv[1][0];
+  
+  int counter = 0;
 
+  do
+  {
+     str = (char)fgetc(f);
+     if(str == moloz)
+        counter++;
+  }while(str != EOF);
 
-        	
-	int counter = 0, i;
-        
-        while(!feof(f)){
-        char *ceva;
-        fscanf(f, "%s\n\n\n", ceva);
-        printf("%s", ceva);
-	int lenght = strlen(ceva);
-	   for(i = 0;i<lenght; i++){
-               if(ceva[i] == moloz)
-                   counter++;
-           }
-        }
+  fclose(f);           
+  printf("Numarul aparitii al lui %c %d", moloz, counter);
 
-        fclose(f);           
-        printf("Numarul aparitii al lui %c %d", moloz, counter);
-
-
-	return 0;
+  return cunter;
 }
